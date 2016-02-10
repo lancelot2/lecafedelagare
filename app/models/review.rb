5827cdcff561995a7345b3fbae69fbaab2553559
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-02-10 15:02:28 +0100

    Improve ratings for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+
+  RATINGS = %w(0 1 2 3 4 5)
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: RATINGS }
 end
